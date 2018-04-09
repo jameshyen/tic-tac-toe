41419ea8a986ffea262731950d59e6adc63599b1
James Yen <jameshyen@gmail.com>
2018-04-09 16:32:36 -0700

    Newline hell

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
   terminal: false,
 });
 
-let userSwitch = false; // Player 1 is false, Player 2 is true.
+let userSwitch = false; // X is false, O is true.
 
 const board = [
   [null, null, null],
@@ -15,17 +15,42 @@ const board = [
   [null, null, null],
 ];
 
+const player = () => userSwitch ? 'O' : 'X';
+
 const printBoard = () => {
-  console.log('Board:');
+  const playerName = player();
+  console.log('Board:\n');
   board.forEach((row) => {
     row.forEach((piece) => {
       process.stdout.write(`${piece} `);
     });
     process.stdout.write('\n');
   });
+  console.log(`\n${playerName}'s turn...`);
 };
 
 rl.on('line', (command) => {
-  console.log(`User move: ${command}`);
-  printBoard();
+  const playerName = player();
+  if (/\d,[ ]?\d/.test(command)) {
+    const matches = command.match(/(\d),[ ]?(\d)/);
+    let [_, x, y] = matches;
+    x = parseInt(x);
+    y = parseInt(y);
+    if (x > 3 || x < 1 || y > 3 || y < 1) {
+      console.log('Your move is out of bounds!');
+    } else {
+      x -= 1;
+      y -= 1;
+      if (!board[x][y]) {
+        board[x][y] = playerName;
+        console.log(`\n${playerName} does: ${command}\n`);
+        userSwitch = !userSwitch;
+        printBoard();
+      } else {
+        console.log('That spot is taken.');
+      }
+    }
+  } else {
+    console.log('Place your move using the following command: x, y.');
+  }
 });
